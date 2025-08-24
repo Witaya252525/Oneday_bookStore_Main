6c69ba21f3172509608e14d60fe8d6db1c65b3d6
witaya007 <witaya.chaison@gmail.ccom>
2025-08-25 01:56:57 +0700

    Aug_25

--- testproject/book/views.py
+++ testproject/book/views.py
@@ -1,44 +1,36 @@
-from django.shortcuts import render ,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .models import Category ,Book ,BookComment
-
+from .models import Category, Book, BookComment
 
 
 def index(request):
-    categ_id = request.GET.get('categoryid')  # Get the category ID from the query parameter
+    # Get the category ID from the query parameter
+    categ_id = request.GET.get('categoryid')
     print(categ_id)
     categories = Category.objects.all()  # Fetch all categories
-    books = Book.objects.filter(category_id=categ_id) if categ_id else Book.objects.all()
-   
+    books = Book.objects.filter(
+        category_id=categ_id) if categ_id else Book.objects.all()
+
     # if categ_id:
     #     books = Book.objects.filter(category_id=categ_id)
     # else:
     #     books = Book.objects.all()
 
-
-
-    
-    paginator = Paginator(books ,8)
+    paginator = Paginator(books, 8)
     page = request.GET.get('page')
     try:
         books = paginator.page(page)
     except PageNotAnInteger:
         books = paginator.page(1)
     except EmptyPage:
-        books = paginator.page(paginator.num_pages) 
+        books = paginator.page(paginator.num_pages)
 
-    
-
-    
-    
     return render(request, 'book/index.html', {
         'categories': categories,
         'books': books,
-        'categ_id': int(categ_id) if categ_id else None  # Pass the category ID to the template
+        # Pass the category ID to the template
+        'categ_id': int(categ_id) if categ_id else None
     })
-
-
-
 
 
 def detail(request, slug):
@@ -46,5 +38,4 @@ def detail(request, slug):
     book = get_object_or_404(Book, slug=slug)
     # Fetch comments using the related_name
     comments = book.bookcomment_set.all()
-    return render(request, 'book/detail.html', {'book': book , 'comments': comments})
-
+    return render(request, 'book/detail.html', {'book': book, 'comments': comments})
